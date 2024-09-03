d611cfffa30e3596c282d3c4cc90d1ec324e7d5e
jcstotts <jcarterstotts@gmail.com>
2024-09-03 21:47:43 +0000

    Changes 9/3/2024 Adding to Fighter class scraping

--- Scraper/Source/Fighters.py
+++ Scraper/Source/Fighters.py
@@ -6,7 +6,8 @@ class Fighter:
         self.Name = ''
         self.Nickname = None
         self.Record = (0,0,0)
-        self.Height = 0
+        self.Height = 0 #Height in inches
+        self.Weight = 0 #Weight in lbs
         self.Reach = 0
         self.Stance = ''
         self.SLpM = 0
@@ -23,7 +24,7 @@ def get_fighter_links():
     fighter_links = []
 
     #Connect to ufcstats.com and create a dictionary for all completed ufc events.
-    for letter in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
+    for letter in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'][0]:
         fighters_URL = 'http://ufcstats.com/statistics/fighters?char=%s&page=all'%letter #Webpage containing all UFC fighters
         page = requests.get(fighters_URL)
         soup = BeautifulSoup(page.content, features='lxml')
@@ -57,9 +58,27 @@ def construct_fighter_objects(fighter_links): #Scrape UFC stats to create fighte
             fighter.Record = tuple([int(x) for x in headers.contents[3].text.split(':')[1].split('\n')[0].split(' ')[1].split('-')])
         else:
             fighter.Record = tuple([int(x) for x in headers.contents[3].text.split(':')[-1].split('-')])
+        if len(fighter_soup.find('p').contents[0].split('\n')) == 3:
+            fighter.Nickname = fighter_soup.find('p').contents[0].split('\n')[1][2:]
+        else:
+            fighter.Nickname = None
+        for element in fighter_soup.find_all('li'):
+            if 'Height' in element.text:
+                if '-' not in element.text:
+                    feet_in = [int(x[:-1]) for x in element.text.split()[1:]]
+                    fighter.Height = 12*feet_in[0] + feet_in[1]
+                else:
+                    fighter.Height = None
+            if 'Weight' in element.text:
+                if '-' not in element.text:
+                    fighter.Weight = int(element.text.split()[1])
+                else:
+                    fighter.Weight = None
+
+        print(fighter.Name, fighter.Height, fighter.Weight)
  
 
-        print(num_fighters, fighter.Name, fighter.Record, vars(fighter))
+        #print(num_fighters, fighter.Name, fighter.Record, vars(fighter))
 
 
 fighter_links = get_fighter_links()
